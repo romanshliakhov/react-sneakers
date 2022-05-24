705cc6a6d752816bf10253e2f3f340e25c9af0d3
Roman <romanshliakhov@gmail.com>
2022-05-24 13:45:15 +0300

    starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Starting</h1>
+      <h2>Старт проекта</h2>
+      <p>Тест отображения на странице</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
